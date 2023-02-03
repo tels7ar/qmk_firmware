af1a0e5f4891bd720d9eecef9c7f2201ed30069a
Phil Hollenback <philiph@pobox.com>
2023-02-02 21:20:41 -0800

    Tweak the config
    Enable all the rgb modes, even the dumb ones.
    
    Add media controls to fn-arrow keys

--- keyboards/massdrop/alt/keymaps/philz/rules.mk
+++ keyboards/massdrop/alt/keymaps/philz/rules.mk
@@ -1,3 +1,4 @@
 WPM_ENABLE = yes
 VELOCIKEY_ENABLE = yes # doesn't work with RGB MATRIX only RGB LIGHT :(
+# enable mode where double tap of left shift makes all keys caps until a word break.
 CAPS_WORD_ENABLE = yes
